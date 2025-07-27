f610a278d1b60b21b8aa774c023a668026ef9b76
abukhairi2 <bukhairi007@gmail.com>
2025-07-27 16:47:01 +0800

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,9 +1,1 @@
-// This optional code is used to register a service worker.
-// register() is called in index.js
-export function register() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js');
-    });
-  }
-}
+
