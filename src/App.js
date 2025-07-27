f06b25157a9c5408c849e15af0104cacb2188f2f
abukhairi2 <bukhairi007@gmail.com>
2025-07-27 16:46:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>Chendering Inn Invoice</h1>
-      <p>This is a simple PWA invoice app.</p>
-    </div>
-  );
-}
-
-export default App;
