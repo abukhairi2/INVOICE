01d7e02b68f3926c7843993ba4b35120c801f458
abukhairi2 <bukhairi007@gmail.com>
2025-07-27 16:00:44 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Chendering Inn Invoice</h1>
+      <p>This is your invoice app. You can now deploy and install it!</p>
+    </div>
+  );
+}
+
+export default App;
