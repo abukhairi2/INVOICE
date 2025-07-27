e138115f5ad6b68d04bd30569bf5eb0f5f37b007
abukhairi2 <bukhairi007@gmail.com>
2025-07-27 16:54:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
 
-function App() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Chendering Inn Invoice</h1>
-      <p>This is a simple PWA invoice app.</p>
-    </div>
-  );
-}
-
-export default App;
